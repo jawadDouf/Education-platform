306981d3022387435ed235ba89c7b52247ade9a2
jawadDouf <jdoufare2001@gmail.com>
2022-11-13 00:22:57 +0100

    SCF-21 finishing select all students methods(students with promotions and without promotions backend

--- src/main/java/com/example/simplon_clone_fb/Dao/daoApprenant.java
+++ src/main/java/com/example/simplon_clone_fb/Dao/daoApprenant.java
@@ -4,6 +4,9 @@ import com.example.simplon_clone_fb.Models.ApprenantsModel;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class daoApprenant extends databaseAccessObject<ApprenantsModel> {
 
 
@@ -37,5 +40,43 @@ public class daoApprenant extends databaseAccessObject<ApprenantsModel> {
         return null;
     }
 
+    //select all learners by promotion and year
+
+    public List<ApprenantsModel> selectAll(int year, int promoId){
+        try{
+            //Start the transaction
+            EntityManager entityManager = entityUtility.getEntityManagerFactory().createEntityManager();
+            //Create the query and return the element from database
+            TypedQuery<ApprenantsModel> query = entityManager.createQuery("SELECT a,b from ApprenantsModel a INNER JOIN a.promoApprenantsById b WHERE b.promoId = :promoId AND b.year = :year",ApprenantsModel.class);
+
+            //Prepare the statement
+            query.setParameter("promoId",promoId);
+            query.setParameter("year",year);
+
+            List<ApprenantsModel> apprenants = query.getResultList();
+            return apprenants;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    //select all learners with out a promotion
+    public List<ApprenantsModel> getAllStudentsWithoutPromotion(){
+        try{
+            //Start the transaction
+            EntityManager entityManager = entityUtility.getEntityManagerFactory().createEntityManager();
+            //Create the query and return the element from database
+            TypedQuery<ApprenantsModel> query = entityManager.createQuery("SELECT a  from ApprenantsModel a  LEFT JOIN a.promoApprenantsById b WHERE b.year = null",ApprenantsModel.class);
+
+            List<ApprenantsModel> apprenants = query.getResultList();
+            return apprenants;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+
+
+    }
 
 }
--- src/main/java/com/example/simplon_clone_fb/Dao/daoFormateur.java
+++ src/main/java/com/example/simplon_clone_fb/Dao/daoFormateur.java
@@ -38,4 +38,5 @@ public class daoFormateur extends databaseAccessObject<FormateursModel>{
     }
 
 
+
 }
