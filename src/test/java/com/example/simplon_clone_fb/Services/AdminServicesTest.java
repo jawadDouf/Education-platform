35fc04918c0af09edc37012678d408ae7b4426a9
jawadDouf <jdoufare2001@gmail.com>
2022-11-16 07:17:33 +0100

    SCF-27 adding the testing for 2 classes

--- src/test/java/com/example/simplon_clone_fb/Services/AdminServicesTest.java
+++ src/test/java/com/example/simplon_clone_fb/Services/AdminServicesTest.java
@@ -1,4 +1,30 @@
+package com.example.simplon_clone_fb.Services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AdminServicesTest {
-  
+
+    @Test
+    void getAll() {
+        assertNotNull(new AdminServices().getAll("formateurs"));
+    }
+
+    @Test
+    void addUser() {
+        assertTrue(new AdminServices().addUser("formateurs","nom","prenom","email","password"));
+    }
+
+    @Test
+    void addPromotion() {
+        assertTrue(new AdminServices().addPromotion("name","size"));
+    }
+
+
+
+    @Test
+    void deleteElement() {
+        assertTrue(new AdminServices().deleteElement("formateurs","1"));
+    }
 }
